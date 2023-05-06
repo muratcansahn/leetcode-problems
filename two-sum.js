832a807e33b06d5f32425d0024e74b537807fed8
muratcansahn <muratcansahin@Murat-MacBook-Air.local>
2023-05-06 17:20:31 +0300

    two sum

---
+++ two-sum.js
@@ -0,0 +1,13 @@
+///two sum
+var twoSum = function (nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+};
+console.log(twoSum([2, 7, 11, 15], 9));
